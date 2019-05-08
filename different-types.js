e90cd5db7bfb4be796e8c6ec43856406bdfff865
Christoph Wong <wong.christoph@gmail.com>
2019-05-08 16:08:38 -0700

    basic practice setup with diy testing framework

---
+++ different-types.js
@@ -0,0 +1,16 @@
+const testCase = require('./test-case').testCase
+// a function that returns the max number of unique types after half is removed
+
+function solution(T) {
+  return 0;
+}
+
+
+
+
+const list = [
+    [solution([0]), 1], 
+    [solution([1, 2]), 2], 
+    [solution([0, 0, 1]), 2]];
+
+testCase(list);
